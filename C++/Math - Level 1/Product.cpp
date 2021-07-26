c9b8c61139887981593275eb580d7c238516150a
Jack Meng <error666jm@gmail.com>
2021-07-26 10:42:42 -0400

    Update Product.cpp

--- C++/Math - Level 1/Product.cpp
+++ C++/Math - Level 1/Product.cpp
@@ -3,3 +3,21 @@
 int product(int a, int b) {
     return a * b;
 }
+
+/*
+//Tested with this code piece and returns 6.
+
+#include <bits\stdc++.h>
+
+using namespace std;
+
+int product(int a, int b)
+{
+    return a * b;
+}
+
+int main()
+{
+    cout << product(2, 3) << endl;
+}
+*/
